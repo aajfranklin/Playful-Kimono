2760b4308e2ac2a27a720499ab33138c571a6988
aajfranklin <aajfranklin@gmail.com>
2020-07-04 11:28:20 +0100

    wip: adds drag and drop file selection to design page

--- src/components/Design/UploadOverlay.js
+++ src/components/Design/UploadOverlay.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { userUploadedImage } from '../../actions/actionCreators';
 
 function UploadOverlay({ userUploadedImage }) {
+  
+  let dragAndDropRef = useRef(null);
+  
+  useEffect(() => {
+    const dragAndDrop = dragAndDropRef.current;
+    dragAndDrop.ondragover = dragAndDrop.ondragenter = (e) => e.preventDefault();
+  
+    dragAndDrop.ondrop = (e) => {
+      e.preventDefault();
+      if (e.dataTransfer.files[0].type.indexOf('image/') !== 0) return;
+      handleFileInput({ target: { files: e.dataTransfer.files } });
+    };
+  }, []);
   
   const handleFileInput = (e) => {
     let reader = new FileReader();
@@ -19,7 +32,7 @@ function UploadOverlay({ userUploadedImage }) {
   }
   
   return(
-    <label id="image-input-label">
+    <label id="image-input-label" ref={dragAndDropRef}>
       <input type="file" id="image-input" accept="image/*" onChange={handleFileInput}/>
       <span id="image-input-text">DRAG AND DROP <br/>OR <u>UPLOAD</u><br/> IMAGE HERE</span>
     </label>
