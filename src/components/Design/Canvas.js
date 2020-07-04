83f6ec7de91de4bc328ca78ecc897a438ee9c1ad
aajfranklin <aajfranklin@gmail.com>
2020-07-04 12:17:15 +0100

    wip: adds zoom buttons to design page

--- src/components/Design/Canvas.js
+++ src/components/Design/Canvas.js
@@ -3,12 +3,14 @@ import { fabric } from 'fabric';
 
 function Canvas ({ userImage }) {
   
+  let canvas;
+  
   /*
     Modified from stack overflow user Matthew's answer for 'How to convert dataURL to file object in Javascript':
     https://stackoverflow.com/questions/6850276/how-to-convert-dataurl-to-file-object-in-javascript
     Licensed under CC BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/legalcode)
    */
-  function dataURItoBlob(dataURI) {
+  const dataURItoBlob = (dataURI) => {
     let byteString = atob(dataURI.split(',')[1]);
     let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
     let arrayBuffer = new ArrayBuffer(byteString.length);
@@ -22,7 +24,7 @@ function Canvas ({ userImage }) {
   }
   
   useEffect(() => {
-    let canvas = new fabric.Canvas('canvas', {
+    canvas = new fabric.Canvas('canvas', {
       width: 500,
       height: 500
     });
@@ -32,14 +34,14 @@ function Canvas ({ userImage }) {
     canvas.setOverlayImage(overlaySrc, () => {
       canvas.overlayImage && canvas.overlayImage.scaleToWidth(500)
       canvas.renderAll();
+  
+      if (userImage) {
+        let img = new fabric.Image(userImage);
+        img.scaleToWidth(canvas.getWidth());
+        canvas.add(img);
+        canvas.item(0).hasControls = canvas.item(0).hasBorders = false;
+      }
     });
-    
-    if (userImage) {
-      let img = new fabric.Image(userImage);
-      img.scaleToWidth(canvas.getWidth());
-      canvas.add(img);
-      canvas.item(0).hasControls = canvas.item(0).hasBorders = false;
-    }
     
     return () => {
       let dataUrl = canvas.toDataURL({
@@ -51,7 +53,33 @@ function Canvas ({ userImage }) {
     }
   }, [userImage]);
   
-  return(<canvas id="canvas"/>)
+  const zoomIn = () => {
+    if (!userImage) return;
+    const img = canvas.item(0);
+    img.scaleX = Math.min(img.scaleX + 0.1, 1);
+    img.scaleY = Math.min(img.scaleY + 0.1, 1);
+    canvas.renderAll();
+  }
+  
+  const zoomOut = () => {
+    if (!userImage) return;
+    const img = canvas.item(0);
+    img.scaleX = Math.max(img.scaleX - 0.1, 0.1);
+    img.scaleY = Math.max(img.scaleY - 0.1, 0.1);
+    canvas.renderAll();
+  }
+  
+  return(
+    <React.Fragment>
+      <canvas id="canvas"/>
+      <div id="zoom">
+        <span>ZOOM</span>
+        <div id="zoom controls">
+          <button onClick={zoomOut} type="button">-</button>
+          <button onClick={zoomIn} type="button">+</button>
+        </div>
+      </div>
+    </React.Fragment>)
 }
 
 export default Canvas;
