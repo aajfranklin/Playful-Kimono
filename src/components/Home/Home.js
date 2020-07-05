61179483b37d29ad9c30633fd479a03a99690e7a
aajfranklin <aajfranklin@gmail.com>
2020-07-06 00:27:55 +0100

    feat: adds first pass at home screen attract animation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Home () {
   
@@ -6,7 +7,21 @@ function Home () {
     document.title = 'Playful Kimono'
   }, [])
   
-  return(<main><h1>Home</h1></main>)
+  return(
+    <main>
+      <section>
+        <div className="fadein">
+          <img id="f1" src="assets/fade1.png"/>
+          <img id="f2" src="assets/fade2.png"/>
+          <img id="f3" src="assets/fade3.png"/>
+          <img id="f4" src="assets/fade4.png"/>
+          <Link to="/design" id="attract-link">
+            <span>CLICK HERE TO START</span>
+          </Link>
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default Home;
