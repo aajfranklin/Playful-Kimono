b975a1740436b270406bee867d1e3ff4de51be9d
aajfranklin <aajfranklin@gmail.com>
2020-07-06 09:49:02 +0100

    feat: adds alt tags to home page images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,10 +11,10 @@ function Home () {
     <main>
       <section>
         <div className="fadein">
-          <img id="f1" src="assets/fade1.png"/>
-          <img id="f2" src="assets/fade2.png"/>
-          <img id="f3" src="assets/fade3.png"/>
-          <img id="f4" src="assets/fade4.png"/>
+          <img id="f1" src="assets/fade1.png" alt="Mountain kimono design"/>
+          <img id="f2" src="assets/fade2.png" alt="Desert cactus kimono design"/>
+          <img id="f3" src="assets/fade3.png" alt="Space shuttle kimono design"/>
+          <img id="f4" src="assets/fade4.png" alt="Torii gate kimono design"/>
           <Link to="/design" id="attract-link">
             <span>CLICK HERE TO START</span>
           </Link>
