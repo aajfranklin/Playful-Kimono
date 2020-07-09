bcd183ca94340bcef2507d1476ab444a5c84ec2a
aajfranklin <aajfranklin@gmail.com>
2020-07-09 09:49:17 +0100

    wip: load more kimonos on reaching end of maximised gallery

--- src/components/Gallery/GalleryControls.js
+++ src/components/Gallery/GalleryControls.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Icon } from '@iconify/react';
 import xIcon from '@iconify/icons-bi/x';
 import chevronCompactRight from '@iconify/icons-bi/chevron-compact-right';
 import chevronCompactLeft from '@iconify/icons-bi/chevron-compact-left';
+import { getKimonos, maximiseKimono, minimiseKimono, minimiseGallery } from '../../actions/actionCreators';
 
-import { maximiseKimono, minimiseKimono, minimiseGallery } from '../../actions/actionCreators';
-
-function GalleryControls({ kimonos, maximisedKimonoIndex, maximiseKimono, minimiseKimono, minimiseGallery }) {
+function GalleryControls({ kimonos, loadedAll, loadingMore, maximisedKimonoIndex, start, getKimonos, maximiseKimono, minimiseKimono, minimiseGallery }) {
+  useEffect(() => {
+    if (maximisedKimonoIndex === kimonos.length - 1 && !loadedAll &&!loadingMore) getKimonos(start);
+  }, [maximisedKimonoIndex, kimonos, loadedAll, loadingMore, getKimonos, start]);
+  
   const changeKimono = (next) => {
     maximiseKimono(next ? maximisedKimonoIndex + 1 : maximisedKimonoIndex - 1);
     minimiseKimono(maximisedKimonoIndex);
@@ -40,12 +43,16 @@ function GalleryControls({ kimonos, maximisedKimonoIndex, maximiseKimono, minimi
 const mapStateToProps = (state) => {
   return {
     kimonos: state.gallery.kimonos,
-    maximisedKimonoIndex: state.gallery.kimonos.findIndex(kimono => kimono.maximised)
+    loadedAll: state.gallery.loadedAll,
+    loadingMore: state.gallery.loadingMore,
+    maximisedKimonoIndex: state.gallery.kimonos.findIndex(kimono => kimono.maximised),
+    start: state.gallery.start
   }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    getKimonos: (start) => dispatch(getKimonos(start)),
     maximiseKimono: (index) => dispatch(maximiseKimono(index)),
     minimiseKimono: (index) => dispatch(minimiseKimono(index)),
     minimiseGallery: () => dispatch(minimiseGallery())
