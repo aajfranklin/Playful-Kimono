250d66a49139239d3afb772461d2da601809de15
aajfranklin <aajfranklin@gmail.com>
2020-07-10 10:29:59 +0100

    fix: missing arrow on mobile devices

--- src/components/Design/SubmittedInstructions.js
+++ src/components/Design/SubmittedInstructions.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
+import arrowRight from '@iconify/icons-bi/arrow-right';
 
 function SubmittedInstructions() {
   return(
@@ -9,7 +11,7 @@ function SubmittedInstructions() {
       <p>Thank you for your patience :)</p>
         <Link to="/gallery">
           <button type="button" id="go-to-gallery" className="action-button available">
-            GO TO GALLERY<br/>🡒
+            GO TO GALLERY<br/><Icon icon={arrowRight}/>
           </button>
         </Link>
     </section>
